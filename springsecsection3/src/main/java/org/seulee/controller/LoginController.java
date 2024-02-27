a44118fef502a74c8f7a68d9c3b81eb0dea6a56c
Lee Seunghyeon <84488007+seulee0862@users.noreply.github.com>
2024-02-27 00:52:04 -0800

    section - user define, user manage (#2)

---
+++ springsecsection3/src/main/java/org/seulee/controller/LoginController.java
@@ -0,0 +1,36 @@
+package org.seulee.controller;
+
+import org.seulee.model.Customer;
+import org.seulee.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        Customer savedCustomer = null;
+        ResponseEntity response = null;
+        try {
+            savedCustomer = customerRepository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given user detials are successfully registered");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occured due to " + ex.getMessage());
+        }
+        return response;
+    }
+}
