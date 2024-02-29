0cf0f166a85e58bf8567030a6465f9d3b37b4c49
Lee Seunghyeon <84488007+seulee0862@users.noreply.github.com>
2024-02-28 20:58:23 -0800

    Section6 (#5)
    * csrf 토큰솔루션 구현
    
    * implemented CSRF toekn solution

---
+++ springsecsection6/src/main/java/org/seulee/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,51 @@
+package org.seulee.config;
+
+import org.seulee.model.Customer;
+import org.seulee.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customers = customerRepository.findByEmail(username);
+
+        if (customers.size() > 0) {
+            if (passwordEncoder.matches(pwd, customers.get(0).getPwd())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+
+                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid password");
+            }
+        } else {
+            throw new BadCredentialsException("No user registered with this details");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
---
+++ springsecsection6/src/main/java/org/seulee/repository/AccountsRepository.java
@@ -0,0 +1,13 @@
+package org.seulee.repository;
+
+
+import org.seulee.model.Accounts;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends CrudRepository<Accounts, Long> {
+
+    Accounts findByCustomerId(int customerId);
+
+}
