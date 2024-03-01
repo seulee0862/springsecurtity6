efebd110d2e34592299369d06ced07cc388f1ab0
이승현 <seulee0862@gmail.com>
2024-02-29 18:52:59 -0800

    add jwt token filter in spring security filter chain

---
+++ springsecsection9/src/main/java/org/seulee/BackendApplication.java
@@ -0,0 +1,17 @@
+package org.seulee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity(debug = true)
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ springsecsection9/src/main/java/org/seulee/controller/CardsController.java
@@ -0,0 +1,30 @@
+package org.seulee.controller;
+
+import org.seulee.model.Cards;
+import org.seulee.repository.CardsRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CardsController {
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @GetMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestParam int id) {
+        List<Cards> cards = cardsRepository.findByCustomerId(id);
+        if (cards != null ) {
+            return cards;
+        }else {
+            return null;
+        }
+    }
+
+}
