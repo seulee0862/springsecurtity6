9857d4ea16f14723cea3594b416aa844282ac981
이승현 <seulee0862@gmail.com>
2024-02-29 17:31:24 -0800

    add custom filter into security filter chain

---
+++ springsecsection8/src/main/java/org/seulee/repository/CardsRepository.java
@@ -0,0 +1,13 @@
+package org.seulee.repository;
+
+import java.util.List;
+
+import org.seulee.model.Cards;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardsRepository extends CrudRepository<Cards, Long> {
+
+    List<Cards> findByCustomerId(int customerId);
+}
