4867749ffe9bae89eb1479fb1de2a3c8aaf7386c
lsh <seulee0862@gmail.com>
2024-03-03 14:45:00 +0900

    use PreAuthorize annotaion in securityFramwork

--- springsecsection10/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ springsecsection10/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -2,9 +2,10 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
