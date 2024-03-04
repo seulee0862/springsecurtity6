3d1eb36c4fba95f2565078a5eee9f9a57be4af71
Lee Seunghyeon <84488007+seulee0862@users.noreply.github.com>
2024-03-04 09:54:29 +0900

    Section8,9 (#10)
    * add custom filter into security filter chain
    
    * add jwt token filter in spring security filter chain
    
    * add jwt authenticate filter in security filter chain

---
+++ springsecsection9/src/main/java/org/seulee/repository/LoanRepository.java
@@ -0,0 +1,13 @@
+package org.seulee.repository;
+import java.util.List;
+
+import org.seulee.model.Loans;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends CrudRepository<Loans, Long> {
+
+    List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+
+}
